251be2573358e9a4cf0730b23a07e92c57eb4d1f
Chirag243p <chirag24.cworld@gmail.com>
2026-03-29 01:19:40 +0530

    fix UI

--- server/db.js
+++ server/db.js
@@ -1,25 +1,10 @@
-const { Pool } = require("pg")
+const { Pool } = require("pg");
 
-const pool = new Pool(process.env.DATABASE_URL ? {
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  },
-  max: 1,
-  idleTimeoutMillis: 10000,
-  connectionTimeoutMillis: 5000
-} : {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 25060,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  max: 1,               // Extremely strict connection limit for DO DBs
-  idleTimeoutMillis: 10000,
-  connectionTimeoutMillis: 5000
-})
+  }
+});
 
-module.exports = pool
+module.exports = pool;
